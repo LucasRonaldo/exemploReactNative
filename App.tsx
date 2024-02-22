b95085f260cbc02ff1fdebf831babe3d38399a12
LucasRonaldo <Lucasronaldoalvesdasilva@gmail.com>
2024-02-22 13:59:37 -0300

    login react native

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { JSXElementConstructor } from 'react';
+import { Text, View } from 'react-native';
+import LoginScreen from './src/login/LoginScreen';
+
+function App():JSX.Element{
+
+  return(
+    <LoginScreen/>
+  );
+}
+
+export default App;
---
+++ src/login/LoginScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function LoginScreen():JSX.Element{
+
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+});
+
+export default LoginScreen;
